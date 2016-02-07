efff4d93481610d3f937e0bb6958a07369508dc7
kidshenlong <michaelpm91@googlemail.com>
2016-02-07 13:38:57 +0000

    First Commit

---
+++ src/app/components/app.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+    selector: 'app',
+    templateUrl: './app/components/app.html',
+    styleUrls: ['./app/components/app.css']
+})
+
+export class AppComponent {}
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by Michael on 07/02/2016.
+ */
+
+import {bootstrap} from 'angular2/platform/browser';
+import {AppComponent} from './app/components/app';
+
+bootstrap(AppComponent);
