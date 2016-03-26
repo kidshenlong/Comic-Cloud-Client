e63b0db007887f90ccdc732c891572e60f6a2e9f
kidshenlong <michaelpm91@googlemail.com>
2016-03-26 15:37:25 +0000

    Initial setup

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Michael on 07/02/2016.
- */
-
-import {bootstrap} from 'angular2/platform/browser';
-import {AppComponent} from './app/components/app';
-
-bootstrap(AppComponent);
