f5a269f141b24dfa8fb1619edd30c45e757662a1
kidshenlong <michaelpm91@googlemail.com>
2016-05-08 01:23:28 +0100

    lol too much Scala.

--- app/shared/data.service.ts
+++ app/shared/data.service.ts
@@ -1,6 +1,6 @@
 /**
  * Created by Michael on 07/05/2016.
  */
-export const API_URL = "http://localhost:3004/"
-export const SERIES_URL = API_URL + "series"
-export const COMICS_URL = API_URL + "comics"
+export const API_URL = "http://localhost:3004/";
+export const SERIES_URL = API_URL + "series";
+export const COMICS_URL = API_URL + "comics";
