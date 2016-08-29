6d98a6379422ccb226791d755655296aacc25850
Michael Patterson-Muir <michael.patterson-muir@bbc.co.uk>
2016-08-29 02:13:22 +0100

    Cleaner redirect logic

--- app/shared/authentication/authentication-guard.service.ts
+++ app/shared/authentication/authentication-guard.service.ts
@@ -8,98 +8,34 @@ import { AuthenticationService } from "./authentication.service";
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
+
+    redirectToLibrary: string[] = ["login", "register"];
+    redirectToLogin: string[] = ["series", "comic", "library"];
+
+
     constructor(private authenticationService: AuthenticationService, private router: Router) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        /*console.log(route);
-        console.log(route.url[0].path);
-
-        if (this.authenticationService.isLoggedIn) { return true; }
-
-        // Store the attempted URL for redirecting
-        this.authenticationService.redirectUrl = state.url;
-
-        // Navigate to the login page
-        this.router.navigate(['/login']);
-        return false;*/
 
         var attemptedPath = route.url[0].path;
 
-        var redirectToLibrary = ["login", "register"];
-        var redirectToLogin = ["series", "comic", "library"];
-
-        console.log(attemptedPath);
-
-       /* if(this.authenticationService.isLoggedIn){return true;}
-
-        // Store the attempted URL for redirecting
-        this.authenticationService.redirectUrl = state.url;
-
-        // Navigate to the login page
-        this.router.navigate(['/login']);
-        return false;*/
-
-
-        /*if(this.authenticationService.isLoggedIn && attemptedPath != "login"){
-            return true;
-        }else if(this.authenticationService.isLoggedIn && attemptedPath == "login"){
-            this.router.navigate(['/library']);
-            return false;
-        }else {
-
-            // Store the attempted URL for redirecting
-            this.authenticationService.redirectUrl = state.url;
-
-            // Navigate to the login page
-            this.router.navigate(['/login']);
-            return false;
-        }*/
-
         if(this.authenticationService.isLoggedIn){
-            console.log("logged in");
-
-            console.log("redirectToLibrary: " + redirectToLibrary);
-
-            if(redirectToLibrary.indexOf(attemptedPath) != -1){
-                console.log("redirect to library");
-                this.router.navigate(['/library']);
-                return false;
-            } else {
-                return true;
-            }
-
-            //if(redirectToLibrary.hasOwnProperty(attemptedPath)){
-            /*if(typeof redirectToLibrary[attemptedPath] === 'undefined'){
-                console.log("redirect to library");
-                this.router.navigate(['/library']);
-                return false;
-            } else {
-                console.log("wuttttt");
-                return true;
-            }*/
+            this.redirectTo(attemptedPath, this.redirectToLibrary, '/library');
         } else {
-            console.log("not logged in");
-            // Store the attempted URL for redirecting
-            //this.authenticationService.redirectUrl = state.url;
-
-            // Navigate to the login page
-            /*this.router.navigate(['/login']);
-            return false;*/
-
-            //console.log("redirectToLogin: " + redirectToLogin);
-
-
-
-            //if(redirectToLogin.hasOwnProperty(attemptedPath)){
-            /*if(typeof redirectToLogin[attemptedPath] === 'undefined'){
-                console.log("redirect to login");
-                this.router.navigate(['/login']);
-                return false;
-            } else {
-                return true;
-            }*/
+            this.redirectTo(attemptedPath, this.redirectToLogin, '/login');
         }
 
-        return true;
     }
+
+    redirectTo(attempted_path: string, paths: string[], redirect_path: string){ //todo better name?
+
+        if(paths.indexOf(attempted_path) != -1){
+            this.router.navigate([redirect_path]);
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
 }
