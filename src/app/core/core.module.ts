e1a64a087689b91e2a6c7bac0dd92ade973ae0df
kidshenlong <michaelpm91@googlemail.com>
2016-11-15 02:12:36 +0000

    Integrating with CLI

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { throwIfAlreadyLoaded } from './module-import-guard';
+import { NavComponent } from './nav/nav.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [NavComponent]
+})
+export class CoreModule {
+  constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
+}
