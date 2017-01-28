d7c3426d7e9d952727829fd1ce709ca0e9a249cc
kidshenlong <michaelpm91@googlemail.com>
2017-01-28 22:14:06 +0000

    Updated Nav. Working Tests.

--- src/app/core/nav/nav.component.spec.ts
+++ src/app/core/nav/nav.component.spec.ts
@@ -11,12 +11,12 @@ import {CoreModule} from "../core.module";
 
 import { ComponentFixtureAutoDetect } from '@angular/core/testing';
 
+let component: NavComponent;
+let fixture: ComponentFixture<NavComponent>;
+let de:      DebugElement;
+let navService: NavService;
+
 describe('NavComponent', () => {
-  let component: NavComponent;
-  let fixture: ComponentFixture<NavComponent>;
-  let de:      DebugElement;
-  //let el:      HTMLElement;
-  let navService: NavService;
 
   beforeEach(async(() => {
     let navServiceStub = {
@@ -25,10 +25,9 @@ describe('NavComponent', () => {
 
     TestBed.configureTestingModule({
       imports: [CoreModule],
-      //declarations: [ NavComponent],
       providers: [
-        { provide: NavService, useValue: navServiceStub }/*,
-        { provide: ComponentFixtureAutoDetect, useValue: true }*/
+        { provide: NavService, useValue: navServiceStub },
+        { provide: ComponentFixtureAutoDetect, useValue: true }
       ]
     })
     .compileComponents();
@@ -36,16 +35,7 @@ describe('NavComponent', () => {
     fixture = TestBed.createComponent(NavComponent);
     component = fixture.componentInstance;
 
-    //navService = fixture.debugElement.injector.get(NavService);
     navService = TestBed.get(NavService);
-
-    //fixture.detectChanges();
-
-    fixture.detectChanges();
-    return fixture.whenStable().then(() => {
-      // 2nd change detection displays the async-fetched hero
-      fixture.detectChanges();
-    });
 
   }));
 
@@ -53,133 +43,34 @@ describe('NavComponent', () => {
 
   });
 
-/*  it('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('first test', () => {
-    expect(de.nativeElement.children.length).toBeGreaterThan(0);
-  });*/
-
-  /*it(' test', () => {
-    expect("").toBe("");
-  });*/
-
-  /*it('second test', async(() => {
-    /!*navService.navState = NavState.Disabled;
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      fixture.detectChanges();
-      fixture.whenStable().then(() => {
-        fixture.detectChanges();
-        console.log(de.nativeElement.children.length);
-        console.log(de.nativeElement);
-        expect(de.nativeElement.children.length).toBe(0);
-      });
-    });
-
-  *!/
-
-    /!*fixture.whenStable().then(() => {
-      navService.navState = NavState.Disabled;
-      fixture.detectChanges();
-      fixture.whenStable().then(() => {
-        fixture.detectChanges();
-        console.log(de.nativeElement);
-      });
-    });*!/
-    navService.navState = NavState.Disabled;
-    //fixture.detectChanges();
-    console.log(de.nativeElement);
-
-  }));*/
-
-  /*it('second test', fakeAsync(() => {
-
-    //navService.navState = NavState.Disabled;
-/!*    fixture.detectChanges();
-    tick();
-    fixture.detectChanges();
-  *!/
-
-
-
-
-    setTimeout(() => console.log(de.nativeElement), 100);
-
-  }));*/
-
-  it('second test', async(() => {
+  it('should be visible if the navState is Unauthorised', async(() => {
     de = fixture.debugElement.query(By.css('.ribbon'));
-
-    //navService.navState = NavState.Authorised;
-    //fixture.detectChanges();
-
-    //console.log(de.nativeElement.children.length);
-    //console.log(de.nativeElement);
     expect(de.nativeElement.children.length).toBeGreaterThan(0);
-
   }));
 
-  it('third test', () => {
-    //de = fixture.debugElement.query(By.css('.ribbon'));
+  it('should not be visible if the navState is Disabled', () => {
 
     navService.navState = NavState.Disabled;
-    //fixture.detectChanges();
 
-    //console.log(de.nativeElement.children.length);
-    /*console.log(de.nativeElement);
-    expect(de.nativeElement.children.length).toBeGreaterThan(0);*/
-
-    //return fixture.whenStable().then(() => {
-      // 2nd change detection displays the async-fetched hero
-      fixture.detectChanges();
+    fixture.detectChanges();
 
     de = fixture.debugElement.query(By.css('.ribbon'));
 
-      expect(de).toBeNull();
-    //});
+    expect(de).toBeNull();
 
   });
 
+  it('should be visible if the navState is Authorised', () => {
 
-/*  it('first test', fakeAsync(() => {
+    navService.navState = NavState.Authorised;
 
-    fixture.detectChanges();
-    tick();
-    fixture.detectChanges();
-    tick();tick();tick();
-    fixture.detectChanges();
+    de = fixture.debugElement.query(By.css('.ribbon'));
 
     expect(de.nativeElement.children.length).toBeGreaterThan(0);
-
-  }));*/
-
-  /*it('third test', fakeAsync(() => {
-    console.log(navService.navState);
-    navService.navState = NavState.Disabled;
-    console.log(navService.navState);
-
-    //expect(de.nativeElement.children.length).toBe(0);
-    /!*fixture.whenStable().then(() => {
-      fixture.detectChanges();
-      fixture.whenStable().then(() => {
-        console.log(de.nativeElement.children.length);
-        expect(de.nativeElement.children.length).toBe(0);
-      });
-    });*!/
-
-    console.log(de.nativeElement);
-    //fixture.detectChanges();
-    tick();                  // wait for async getQuote
-    fixture.detectChanges(); // update view with quote
-    //tick();
-    //tick();
-    //tick();
-    //fixture.detectChanges();
-    console.log(de.nativeElement);
-    expect(de.nativeElement.children.length).toBe(0);
-  }));*/
-
+  });
 
 });
