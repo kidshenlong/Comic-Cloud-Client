c5103d53def20ace08620b9bb4e08f3dae4d40ef
kidshenlong <michaelpm91@googlemail.com>
2017-03-20 20:33:22 +0000

    Search stuff.

--- src/app/comics/comics.module.ts
+++ src/app/comics/comics.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ComicComponent} from "./comic/comic.component";
+import {ComicImageComponent} from "./comic-image/comic-image.component";
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    ComicComponent
+    ComicComponent, ComicImageComponent
   ],
   exports: [
     ComicComponent
