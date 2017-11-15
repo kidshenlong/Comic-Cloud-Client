fcff5d35c5170988cdb4671be90df75c4ed1f5b8
kidshenlong <michaelpm91@googlemail.com>
2017-11-15 00:24:50 +0000

    Nav change

--- src/app/core/nav/nav.component.ts
+++ src/app/core/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavService } from './shared/nav.service';
 import { NavState } from './shared/nav-state.enum';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,5 +12,5 @@ export class NavComponent {
 
   navStateType = NavState;
 
-  constructor(public navService: NavService) { }
+  constructor(public navService: NavService, private router: Router) { }
 }
